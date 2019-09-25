2cf8ab6c1ee885ede0f2ce525c511e913edd5f81
Thimmy Stenlund <ts222ub@student.lnu.se>
2019-09-25 18:54:36 +0200

    Added base for C# project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1dv607_WS2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
